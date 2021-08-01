87ad89f2aa995c709070384c54816b436ddce74c
RAQIB404 <raqibkhan2332@gmail.com>
2021-08-01 10:08:56 +0530

    array

---
+++ index.js
@@ -0,0 +1,50 @@
+//accesing the elements of an array
+var animal = ["tiger","lion","leopard","bear"];
+console.log(animal[0]);
+console.log(animal[2]);
+
+//length of an array
+var animal=["tiger","lion","leopard","bear"];
+console.log(animal.length);
+
+//adding new elements to an array
+var colors=["green","yellow","purple",];
+colors.push("red");
+console.log(colors);
+//prints:red,green,yellow,purple
+
+//removing elements from an array
+var colors =["green","yellow","purple"];
+colors.pop();
+console.log(colors);
+//prints:["green","yellow"]
+
+//removing first element 
+var colors =["green","yellow","purple"];
+colors.shift();
+console.log(colors);
+//prints:["yellow","purple"]
+
+//adding a new element at the beginning
+var colors =["green","yellow","purple"];
+colors.unshift("red");
+console.log(colors);
+//prints:red,green,yellow,purple
+
+//adding or removing elements at any position
+var colors =["green","yellow","purple"];
+colors.splice(0,1);
+console.log(colors);
+//prints:yellow,blue
+
+//addind elements at any position
+colors.splice(1,0,"red");
+console.log(colors);
+//prints:yellow,red,blue
+
+
+//iterating over an array
+var animals =["tiger","lion","leopard","bear","dog","cat","bull","elephant","horse","girrafe"];
+for(var i=0;i<=animals.length -1; i++){
+    console.log(animals[i])
+}
